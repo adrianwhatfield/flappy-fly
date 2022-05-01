480006d827dc4b7c80961324a9a28db05f484dc1
Adrian Hatfield <adrianhatfield23@gmail.com>
2022-05-01 13:34:35 +0100

    Make player fall

---
+++ flappy-fly/scenes/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+const UP = Vector2(0,-1)
+const JUMP = 100
+const GRAVITY = 2
+const MAXFALLSPEED = 200
+
+var motion = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	motion.y += GRAVITY + delta
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	
+	motion = move_and_slide(motion, Vector2.UP)
